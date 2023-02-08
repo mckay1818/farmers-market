7effaa23b24e5e91fecb9697159c2cddfa3cdc56
mckay <mckaybwarren@gmail.com>
2023-02-08 11:44:18 -0500

    add pytest fixtures for creating 1 customer and authenticating 1 customer

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ from app import create_app
 from app import db
 from app.models.seller import Seller
 from app.models.product import Product
+from app.models.customer import Customer
 from flask.signals import request_finished
 
 SELLER_1_STORE_NAME = "Green Acres"
@@ -17,6 +18,17 @@ SELLER_1_ADDRESS_1 = "278 Armstrong Rd"
 SELLER_1_CITY = "Hudson"
 SELLER_1_REGION = "New York"
 SELLER_1_POSTAL_CODE = 12534
+
+CUSTOMER_1_ID = 1
+CUSTOMER_1_USERNAME = "grocerygetter11"
+CUSTOMER_1_FIRST_NAME = "Lux"
+CUSTOMER_1_LAST_NAME = "Sanders"
+CUSTOMER_1_EMAIL = "luxsanders@fakemail.com"
+CUSTOMER_1_PASSWORD = "secretword"
+CUSTOMER_1_ADDRESS_1 = "443 Cherry Lane"
+CUSTOMER_1_CITY = "Hudson"
+CUSTOMER_1_REGION = "New York"
+CUSTOMER_1_POSTAL_CODE = 12534
 
 # setup application instance
 @pytest.fixture
@@ -84,7 +96,6 @@ def seller_access_token(app, client, one_seller):
     access_token = response.get_json()["access_token"]
     return access_token
 
-
 @pytest.fixture
 def one_saved_product(app, one_seller):
     new_product = Product(
@@ -97,3 +108,28 @@ def one_saved_product(app, one_seller):
     )
     db.session.add(new_product)
     db.session.commit()
+
+@pytest.fixture
+def one_customer(app):
+    new_customer = Customer(
+        username=CUSTOMER_1_USERNAME,
+        first_name=CUSTOMER_1_FIRST_NAME,
+        last_name=CUSTOMER_1_LAST_NAME,
+        email=CUSTOMER_1_EMAIL,
+        password=CUSTOMER_1_PASSWORD,
+        address_1=CUSTOMER_1_ADDRESS_1,
+        city=CUSTOMER_1_CITY,
+        region=CUSTOMER_1_REGION,
+        postal_code=CUSTOMER_1_POSTAL_CODE
+    )
+    db.session.add(new_customer)
+    db.session.commit()
+
+@pytest.fixture
+def customer_access_token(app, client, one_customer):
+    response = client.post("/login", json={
+        "email": CUSTOMER_1_EMAIL,
+        "password": CUSTOMER_1_PASSWORD,
+    })
+    access_token = response.get_json()["access_token"]
+    return access_token
